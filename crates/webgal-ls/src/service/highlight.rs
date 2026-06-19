4cce95a01d15486d48ccd0957923d47c2b48c870
fltLi <ddsk_06294219mr@qq.com>
2026-06-19 22:22:46 +0800

    :bug: 修复代码高亮
    - :memo: 在文档中标记语义高亮状态为正常

--- crates/webgal-ls/src/service/highlight.rs
+++ crates/webgal-ls/src/service/highlight.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 use tower_lsp::lsp_types::*;
 use webgal_model::sentence::{Scene, SentenceInfo};
 
@@ -16,7 +18,7 @@ pub fn highlight_capability() -> SemanticTokensServerCapabilities {
             semantic_tokens_options: SemanticTokensOptions {
                 work_done_progress_options: WorkDoneProgressOptions::default(),
                 legend: SemanticTokensLegend {
-                    token_types: TOKEN_TYPES.to_vec(),
+                    token_types: TokenType::all().to_vec(),
                     token_modifiers: vec![],
                 },
                 range: Some(false),
@@ -30,72 +32,138 @@ pub fn highlight_capability() -> SemanticTokensServerCapabilities {
 /// 为场景提供语义高亮
 pub fn highlight(scene: &Scene) -> Vec<SemanticToken> {
     let mut tokens = Vec::new();
+    let mut last_line = 0;
+
     for (line, sentence) in scene.sentences().iter().enumerate() {
-        highlight_sentence(sentence, line, &mut tokens);
+        let mut last_end = 0;
+        tokens.extend(highlight_sentence(sentence).into_iter().map(
+            |PrimaryToken { start, end, kind }| {
+                let delta_line = if line == last_line {
+                    0
+                } else {
+                    let delta = (line - last_line) as u32;
+                    last_line = line;
+                    delta
+                };
+
+                let delta_start = (start - last_end) as u32;
+                let length = (end - start) as u32;
+                last_end = end;
+
+                SemanticToken {
+                    delta_line,
+                    delta_start,
+                    length,
+                    token_type: kind.to_id(),
+                    token_modifiers_bitset: 0,
+                }
+            },
+        ));
     }
+
     tokens
 }
 
 /// 生成一条语句的高亮
-fn highlight_sentence(sentence: &SentenceInfo, line: usize, tokens: &mut Vec<SemanticToken>) {
+fn highlight_sentence(sentence: &SentenceInfo) -> Vec<PrimaryToken> {
     let SentenceInfo {
-        primary, sentence, ..
+        content,
+        primary,
+        sentence,
+        ..
     } = sentence;
 
-    let make_token = |text, kind| {
-        let span = primary.get_span(text);
-        SemanticToken {
-            delta_line: line as u32,
-            delta_start: span.start as u32,
-            length: span.len() as u32,
-            token_type: token_type_id_of(&kind),
-            token_modifiers_bitset: 0,
-        }
+    let mut tokens = Vec::new();
+    let mut push_token = |text, kind| {
+        let ops::Range { start, end } = primary.get_span(text);
+        tokens.push(PrimaryToken { start, end, kind });
     };
 
     // 语句类型高亮
-    tokens.push(make_token(
+    push_token(
         primary.command,
         if sentence.is_say() {
-            SemanticTokenType::VARIABLE
+            TokenType::Variable
         } else {
-            SemanticTokenType::FUNCTION
+            TokenType::Function
         },
-    ));
+    );
 
     // 主参数高亮
     if let Some(content) = primary.content
         && sentence.is_say()
     {
-        tokens.push(make_token(content, SemanticTokenType::STRING));
+        push_token(content, TokenType::String);
     }
 
     // 参数高亮
-    tokens.extend(
-        primary
-            .arguments
-            .iter()
-            .map(|&(name, _)| make_token(name, SemanticTokenType::PROPERTY)),
-    );
+    for (name, _) in primary.arguments.iter() {
+        push_token(name, TokenType::Property);
+    }
 
     // 注释高亮
     if !primary.comment.is_empty() {
-        tokens.push(make_token(primary.comment, SemanticTokenType::COMMENT));
+        let comment = content
+            .len()
+            .checked_sub(primary.comment.len() + 1)
+            .and_then(|pos| {
+                let comment = &content[pos..]; // 尝试包含前导 `;`
+                comment.starts_with(';').then_some(comment)
+            })
+            .unwrap_or(primary.comment);
+        push_token(comment, TokenType::Comment);
+    }
+
+    tokens
+}
+
+#[derive(Clone, Copy)]
+struct PrimaryToken {
+    start: usize,
+    end: usize,
+    kind: TokenType,
+}
+
+#[derive(Clone, Copy)]
+enum TokenType {
+    Function,
+    Variable,
+    Property,
+    String,
+    Comment,
+}
+
+impl TokenType {
+    fn to_id(self) -> u32 {
+        match self {
+            Self::Function => 0,
+            Self::Variable => 1,
+            Self::Property => 2,
+            Self::String => 3,
+            Self::Comment => 4,
+        }
+    }
+
+    const fn all() -> &'static [SemanticTokenType] {
+        const TOKEN_TYPES: &[SemanticTokenType] = &[
+            SemanticTokenType::FUNCTION,
+            SemanticTokenType::VARIABLE,
+            SemanticTokenType::PROPERTY,
+            SemanticTokenType::STRING,
+            SemanticTokenType::COMMENT,
+        ];
+        TOKEN_TYPES
     }
 }
 
-const TOKEN_TYPES: &[SemanticTokenType] = &[
-    SemanticTokenType::FUNCTION,
-    SemanticTokenType::VARIABLE,
-    SemanticTokenType::PROPERTY,
-    SemanticTokenType::STRING,
-    SemanticTokenType::COMMENT,
-];
-
-fn token_type_id_of(token_type: &SemanticTokenType) -> u32 {
-    TOKEN_TYPES
-        .iter()
-        .enumerate()
-        .find_map(|(id, kind)| (kind == token_type).then_some(id))
-        .unwrap() as u32
+impl From<TokenType> for SemanticTokenType {
+    fn from(value: TokenType) -> Self {
+        match value {
+            TokenType::Function => Self::FUNCTION,
+            TokenType::Variable => Self::VARIABLE,
+            TokenType::Property => Self::PROPERTY,
+            TokenType::String => Self::STRING,
+            TokenType::Comment => Self::COMMENT,
+        }
+    }
 }
