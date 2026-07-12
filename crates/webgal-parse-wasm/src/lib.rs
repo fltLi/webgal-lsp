5b8f08be0283a917fdff11ba6a4fd4d6b411165c
fltLi <ddsk_06294219mr@qq.com>
2026-07-12 16:55:04 +0800

    :pencil2: 更改 WASM 封装中错误的返回类型描述

--- crates/webgal-parse-wasm/src/lib.rs
+++ crates/webgal-parse-wasm/src/lib.rs
@@ -2,14 +2,14 @@ use serde::Serialize;
 use wasm_bindgen::prelude::*;
 use webgal_model::sentence::SentenceInfo;
 
-/// 解析场景, 以 JSON 格式返回数据结构
+/// 解析场景, 以 [`JsValue`] 格式返回数据结构
 #[wasm_bindgen]
 pub fn parse_scene(text: &str) -> Result<JsValue, JsValue> {
     let scene: Vec<_> = text.lines().map(SentenceInfo::from_str).collect();
     serialize(&scene)
 }
 
-/// 解析语句, 以 JSON 格式返回数据结构
+/// 解析语句, 以 [`JsValue`] 格式返回数据结构
 #[wasm_bindgen]
 pub fn parse_sentence(text: &str) -> Result<JsValue, JsValue> {
     let sentence = SentenceInfo::from_str(text);
