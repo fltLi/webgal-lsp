9f6fc759d9d6cd31a39736ae0927747b88864aff
fltLi <ddsk_06294219mr@qq.com>
2026-07-12 21:30:14 +0800

    :sparkles: 导出场景语义高亮为独立模块, 并实现 WASM 封装

--- crates/webgal-parse-wasm/src/lib.rs
+++ crates/webgal-parse-wasm/src/lib.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "highlight")]
+use lsp_types::SemanticToken;
 use serde::Serialize;
 use wasm_bindgen::prelude::*;
 use webgal_model::sentence::SentenceInfo;
@@ -17,8 +19,57 @@ pub fn parse_sentence(text: &str) -> Result<JsValue, JsValue> {
     serialize(&sentence)
 }
 
+/// 提供场景语义高亮, 返回 [`Vec<lsp_types::SemanticToken>`]
+#[cfg(feature = "highlight")]
+#[wasm_bindgen]
+pub fn highlight_scene(text: &str) -> Result<JsValue, JsValue> {
+    use webgal_highlight::highlight;
+    use webgal_model::sentence::Scene;
+
+    let scene = Scene::from_str(text);
+    let tokens = highlight(&scene);
+
+    serialize(
+        &tokens
+            .into_iter()
+            .map(SemanticTokenHelper::from)
+            .collect::<Vec<_>>(),
+    )
+}
+
 // -------- util --------
 
 fn serialize<T: Serialize>(value: &T) -> Result<JsValue, JsValue> {
     serde_wasm_bindgen::to_value(value).map_err(From::from)
+}
+
+#[cfg(feature = "highlight")]
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct SemanticTokenHelper {
+    delta_line: u32,
+    delta_start: u32,
+    length: u32,
+    token_type: u32,
+    token_modifiers_bitset: u32,
+}
+
+#[cfg(feature = "highlight")]
+impl From<SemanticToken> for SemanticTokenHelper {
+    fn from(value: SemanticToken) -> Self {
+        let SemanticToken {
+            delta_line,
+            delta_start,
+            length,
+            token_type,
+            token_modifiers_bitset,
+        } = value;
+        Self {
+            delta_line,
+            delta_start,
+            length,
+            token_type,
+            token_modifiers_bitset,
+        }
+    }
 }
